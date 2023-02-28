4e2679241846a7781763cf06c25e6141a4381c4d
HeitorFernandes <heitorsoaresfer@gmail.com>
2023-02-28 19:37:11 -0300

    [Feat] Cria validação de email e senha; Conserta erros de lint

--- back-end/src/api/Service/user.service.js
+++ back-end/src/api/Service/user.service.js
@@ -1,49 +1,50 @@
-const { User } = require('../../database/models')
-const errorHandler = require('../../middleware/error.middleware')
-const bcrypt = require('bcryptjs')
-const { createToken } = require('../../utils/jwt')
-const  NotFoundError = require('../../utils/errors/notFoundError')
-const ConflictError = require('../../utils/errors/conflictError')
+const bcrypt = require('bcryptjs');
+const { validate } = require('email-validator');
+const { User } = require('../../database/models');
+const { createToken } = require('../../utils/jwt');
+const NotFoundError = require('../../utils/errors/notFoundError');
+const ConflictError = require('../../utils/errors/conflictError');
 
  const loginUser = async (email, password) => {
         const user = await User.findOne({
-            where: { email }
-        })  
-        if(!user) {
-            throw new NotFoundError('Not Found')
+            where: { email },
+        });  
+        if (!user) {
+            throw new NotFoundError('Not Found');
         }
 
-        const decode = bcrypt.compareSync(password, user.password)
+        const decode = bcrypt.compareSync(password, user.password);
         
         if (!decode) {
-            throw new NotFoundError('Not Found')
+            throw new NotFoundError('Not Found');
         }
-        const token = createToken(email)
-        return { user, token }
-}
+        const token = createToken(email);
+        return { user, token };
+};
 
- const registerUser = async (name, email, password, role = 'client' ) => {
+ const registerUser = async (name, email, password, role = 'client') => {
      const user = await User.findOne({
-         where: { email }
-        })  
+         where: { email },
+        });  
     if (user) {
-        throw new ConflictError( 'Conflict')
+        throw new ConflictError('Conflict');
     }
-    const encryptedPassword = bcrypt.hashSync(password)
+    if (!validate(email) || password.length < 6) {
+        throw new ConflictError('Invalid email or password');
+      }
+    const encryptedPassword = bcrypt.hashSync(password);
 
     await User.create({
         name,
         email,
         password: encryptedPassword,
-        role
-    })
+        role,
+    });
 
-    return 'user created'
-
- }
-
+    return 'user created';
+ };
 
 module.exports = {
     loginUser,
-    registerUser
-}
+    registerUser,
+};
