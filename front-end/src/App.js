5afad9def5cb00bed91447d3db3b5e6afb4b9e66
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2023-03-02 19:11:58 -0300

    [Feat] Implementação da nav bar do fluxo de clientes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import CustomerProducts from './pages/CustomerProducts';
 import Provider from './context/Provider';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" render={ () => (<Redirect to="/login" />) } />
         <Route exact path="/login" component={ Login } />
         <Route exact path="/register" component={ Register } />
+        <Route exact path="/customer/products" component={ CustomerProducts } />
       </Switch>
     </Provider>
   );
