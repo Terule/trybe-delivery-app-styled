19ac797a2e96e92857ef9f8b857bec439b4a038f
Giovanni <giovannibsf@gmail.com>
2023-03-07 17:00:06 -0300

    [FEAT]: rota para buscar a sale pelo sellrId

--- back-end/src/api/Routes/sale.router.js
+++ back-end/src/api/Routes/sale.router.js
@@ -1,9 +1,10 @@
 const Router = require('express');
-const { newSale, getAllSales, getSaleById } = require('../Controller/sale.controller');
+const { newSale, getAllSales, getSaleById, getSaleBySellerId } = require('../Controller/sale.controller');
 
 const route = Router();
 
 route.post('/sales', newSale);
+route.get('/sales', getSaleBySellerId),
 route.get('/sales/:id', getSaleById);
 route.get('/customer/orders', getAllSales);
 
