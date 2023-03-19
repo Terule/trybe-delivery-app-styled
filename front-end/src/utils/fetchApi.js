174e1fa2bf10318f80e94ff97fae3f5caa375937
Rafael Aguiar <terule@gmail.com>
2023-03-19 15:22:27 -0300

    [Fix] port error on fetch1

--- front-end/src/utils/fetchApi.js
+++ front-end/src/utils/fetchApi.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const HOST = process.env.REACT_APP_HOSTNAME || 'localhost';
-const PORT = process.env.REACT_APP_BACKEND_PORT || 3001;
+// const PORT = process.env.REACT_APP_BACKEND_PORT || 3001;
 const PROTOCOL = process.env.REACT_APP_PROTOCOL || 'http';
 
-const BASE_URL = `${PROTOCOL}://${HOST}:${PORT}`;
+const BASE_URL = `${PROTOCOL}://${HOST}`;
 
 const api = axios.create({
   baseURL: BASE_URL,
