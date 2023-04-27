dfc1ddafc59fff67f973b7861d9d648fcaa49fe6
Rafael Aguiar <terule@gmail.com>
2023-04-27 09:52:10 -0300

    fix: 🐛 table

--- back-end/src/database/models/SalesProducts.js
+++ back-end/src/database/models/SalesProducts.js
@@ -20,21 +20,5 @@ module.exports = (sequelize, DataTypes) => {
     tableName: 'sales_products',
   });
 
-  salesProducts.associate = (models) => {
-    models.Sale.belongsToMany(models.Product, {
-      as: 'products',
-      through: salesProducts,
-      foreignKey: 'saleId',
-      otherKey: 'productId',
-    });
-
-    models.Product.belongsToMany(models.Sale, {
-      as: 'sales',
-      through: salesProducts,
-      foreignKey: 'productId',
-      otherKey: 'saleId',
-    });
-  };
-
   return salesProducts;
 }
