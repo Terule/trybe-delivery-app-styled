83d1e900e2f2cffd326b4b37044202734d0c45ff
Rafael Aguiar <terule@gmail.com>
2023-04-27 09:31:27 -0300

    fix: 🐛 ssl

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -18,8 +18,13 @@ const options = {
   username: process.env.MYSQLUSER || process.env.MYSQL_USER || 'root',
   password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
+  ssl: true,
   dialectOptions: {
     timezone: 'Z',
+    ssl: {
+      require: true,
+      rejectUnauthorized: true,
+    }
   },
   logging: false,
 };
